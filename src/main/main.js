3782c61890a0db7d9e7025cf01f06bc3150d2e48
Deadlypinfish <Deadlypinfish@gmail.com>
2023-06-19 22:52:52 -0400

    starting flex layout

--- src/main/main.js
+++ src/main/main.js
@@ -25,7 +25,7 @@ const createMainWindow = () => {
   
     mainWindow.loadFile("src/renderer/mainWindow/index.html");    
     
-    //mainWindow.webContents.openDevTools();
+    mainWindow.webContents.openDevTools();
   
 };
 
