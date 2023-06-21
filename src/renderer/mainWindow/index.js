4cc4d9c5c65d93b99b3803e4f881b0ce76b81a19
Deadlypinfish <Deadlypinfish@gmail.com>
2023-06-20 22:46:47 -0400

    spinner graphic

--- src/renderer/mainWindow/index.js
+++ src/renderer/mainWindow/index.js
@@ -6,6 +6,12 @@ document
     event.preventDefault();
     let userInput = document.getElementById("user-input").value;
 
+    var spinner = document.getElementById('spinner');
+    var submitButton = document.getElementById('btn-submit-message');
+
+    submitButton.style.display = 'none';
+    spinner.style.display = 'block';
+
     // Send 'run-query' event to the main process with the user input as argument
     ipcRenderer.send("run-query", userInput);
   });
@@ -14,5 +20,14 @@ document
 ipcRenderer.on("api-response", (event, message) => {
   console.log("api-response called");
   console.log("message:" + message);
+
+  var spinner = document.getElementById('spinner');
+  var submitButton = document.getElementById('btn-submit-message');
+
+  submitButton.style.display = 'block';
+  spinner.style.display = 'none';
+
+  document.getElementById("user-input").value = '';
+  
   document.getElementById("chat-response").innerText = message.content;
 });
